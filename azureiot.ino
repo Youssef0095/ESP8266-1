dc17f5330f099771f6d00724063bc7721bfabd4a
Youssef0095 <80364160+Youssef0095@users.noreply.github.com>
2021-03-12 15:30:22 +0100

    Update azureiot.ino
    Updated

--- azureiot.ino
+++ azureiot.ino
@@ -10,13 +10,15 @@ void sendCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void *userContextCal
   messagePending = false;
 }
 
-void sendMessage(char *payload) {
+void sendMessage(char *payload, char *epochTime) {
   IOTHUB_MESSAGE_HANDLE message = IoTHubMessage_CreateFromByteArray((const unsigned char *) payload, strlen(payload));
 
   MAP_HANDLE properties = IoTHubMessage_Properties(message);
-  Map_Add(properties, "Skola", "Nackademin");
-  Map_Add(properties, "MittNamn", "Youssef");
-
+  Map_Add(properties, "deviceType", "dht");
+  Map_Add(properties, "latitude", "59.2465954");
+  Map_Add(properties, "longitude", "18.0609175");
+  Map_Add(properties, "epochTime", epochTime);
+  
   if(IoTHubClient_LL_SendEventAsync(deviceClient, message, sendCallback, NULL) == IOTHUB_CLIENT_OK) {
     messagePending = true;
   }
